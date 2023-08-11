aed27fbf3ddc8a6e0d29925b7908713ce50767a3
GitHub Action <action@github.com>
2023-08-11 15:48:48 +0000

    Template cleanup

--- src/main/kotlin/com/github/koterra/intellijdoom/services/MyProjectService.kt
+++ src/main/kotlin/com/github/koterra/intellijdoom/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.koterra.intellijdoom.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.koterra.intellijdoom.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
