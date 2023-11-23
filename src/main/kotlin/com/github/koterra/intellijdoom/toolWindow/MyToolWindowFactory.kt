ad07852341e9a1b65e48a118c80f48583ea29ef6
KOTerra <82443072+KOTerra@users.noreply.github.com>
2023-11-24 01:17:12 +0200

    demo

--- src/main/kotlin/com/github/koterra/intellijdoom/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/koterra/intellijdoom/toolWindow/MyToolWindowFactory.kt
@@ -1,5 +1,7 @@
 package com.github.koterra.intellijdoom.toolWindow
 
+import com.github.koterra.intellijdoom.MyBundle
+import com.github.koterra.intellijdoom.services.MyProjectService
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
@@ -8,8 +10,10 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import com.github.koterra.intellijdoom.MyBundle
-import com.github.koterra.intellijdoom.services.MyProjectService
+import com.intellij.ui.jcef.JBCefApp
+import com.intellij.ui.jcef.JBCefBrowser
+import org.cef.browser.CefBrowser
+import java.awt.BorderLayout
 import javax.swing.JButton
 
 
@@ -32,14 +36,38 @@ class MyToolWindowFactory : ToolWindowFactory {
         private val service = toolWindow.project.service<MyProjectService>()
 
         fun getContent() = JBPanel<JBPanel<*>>().apply {
-            val label = JBLabel(MyBundle.message("randomLabel", "?"))
+            //val label = JBLabel(MyBundle.message("randomLabel", "?"))
 
-            add(label)
-            add(JButton(MyBundle.message("shuffle")).apply {
-                addActionListener {
-                    label.text = MyBundle.message("randomLabel", service.getRandomNumber())
-                }
-            })
+            //add(label)
+            /*  add(JButton(MyBundle.message("shuffle")).apply {
+                  addActionListener {
+                      label.text = MyBundle.message("randomLabel", service.getRandomNumber())
+                  }
+              })*/
+
+
+            val panel = JBPanel<JBPanel<*>>()
+            panel.layout = BorderLayout()
+
+            // Create a JBCefBrowser
+            val cefApp = JBCefApp.getInstance()
+            val cefBrowser = JBCefBrowser()
+
+            // Load a URL
+            cefBrowser.loadURL("https://dos.zone/player/?bundleUrl=https%3A%2F%2Fcdn.dos.zone%2Fcustom%2Fdos%2Fdoom.jsdos?anonymous=1")
+
+            // Add the browser component to the panel
+            panel.add(cefBrowser.component, BorderLayout.CENTER)
+
+            // Add a button to demonstrate interaction
+            val button = JButton("Click me")
+            button.addActionListener {
+                // Execute some JavaScript on the browser
+              //  cefBrowser.executeJS("alert('Button clicked!');")
+            }
+            panel.add(button, BorderLayout.SOUTH)
+
+            return panel
         }
     }
 }
