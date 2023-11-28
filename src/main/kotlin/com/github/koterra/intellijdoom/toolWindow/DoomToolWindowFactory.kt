c87ad155e8bf05023326dcab13e5c5f26e157421
KOTerra <82443072+KOTerra@users.noreply.github.com>
2023-11-28 19:16:11 +0200

    organise imports

--- src/main/kotlin/com/github/koterra/intellijdoom/toolWindow/DoomToolWindowFactory.kt
+++ src/main/kotlin/com/github/koterra/intellijdoom/toolWindow/DoomToolWindowFactory.kt
@@ -7,19 +7,10 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.ComboBox
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
-import com.intellij.openapi.wm.ToolWindowManager
-import com.intellij.openapi.wm.WindowManager
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import com.intellij.ui.content.ContentManagerAdapter
-import com.intellij.ui.content.ContentManagerEvent
-import com.intellij.ui.content.ContentManagerListener
 import com.intellij.ui.jcef.JBCefBrowser
-import org.cef.browser.CefBrowser
-import org.cef.handler.CefFocusHandler
-import org.cef.handler.CefFocusHandlerAdapter
 import java.awt.BorderLayout
-import java.awt.event.FocusListener
 
 
 class DoomToolWindowFactory : ToolWindowFactory {
